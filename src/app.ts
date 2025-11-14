7fc07881849774d87c4d401238e57f3323ae3a77
Victor Lacomb de Melo <57971012+lacomb04@users.noreply.github.com>
2025-11-14 15:28:22 +0000

    atualizado para deploy

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,2 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Tickets from './pages/Tickets';
-import AdminDashboard from './pages/AdminDashboard';
-
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tickets" element={<Tickets />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-      </Routes>
-    </Router>
-  );
-};
-
-export default App;
+// Re-export the main App component using a TS-only module to avoid JSX in a .ts file.
+export { default } from "./App";
